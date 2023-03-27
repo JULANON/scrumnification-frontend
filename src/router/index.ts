d8d33c2157e50ce46c7b44e67702a1f0ee054df5
63160289 <63160289@go.buu.ac.th>
2023-03-27 22:38:46 +0700

    edit frontend 27/3/2566 22.37

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,7 +10,7 @@ const router = createRouter({
 
     },
     {
-      path: "/",
+      path: "/login",
       name: "login",
       component: () => import("../views/LoginView.vue"),
       meta:{
---
+++ src/views/Store/test.vue
@@ -0,0 +1,39 @@
+<div class="d-flex justify-content-between">
+
+    <div class="p-2 flex-fill bd-highlight logo-index" >
+      <img id="icon" src="../assets/icon/Pink_Elegant_Lash_Makeup_Brow_artist_Beauty_Logo__1_-removebg-preview.png">
+    </div>
+
+  <div class="p-2 flex-fill bd-highlight center">
+    <div class="login-page">
+      <div class="form">
+        <!-- <button @click="login()">login</button> -->
+        <!-- try this! -->
+        <br>
+        <!-- username: admin, password: root
+        username:coachc0, password: root
+        username: leaderc0, password: root -->
+        <form class="register-form">
+          <input type="text" placeholder="name"/>
+          <input type="password" placeholder="password"/>
+          <input type="text" placeholder="email address"/>
+
+          <button>create</button>
+        </form>
+        <form class="login-form">
+          <input type="text" placeholder="username" v-model="username"/>
+          <input type="password" placeholder="password" v-model="password"/>
+
+          <router-link to="/home">
+            <!-- <button @click="loginAdmin()">login Admin</button>
+            <button @click="loginLeader()">login Leader</button>
+            <button @click="loginCoach()">login Coach</button> -->
+            <button @click="login()">login</button>
+          </router-link>
+          <!-- <p class="message">ขี้เกียจ login? <router-link to="/home" >Click me</router-link></p> -->
+
+        </form>
+      </div>
+    </div>
+    </div>
+  </div>
